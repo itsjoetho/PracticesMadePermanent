890bc90621ada34b406be2010f416ae114a9da8c
itsjoetho <iamjoellynch@gmail.com>
2023-11-06 01:10:57 -0500

    I commited these files on 11/04/2023

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
+import java.awt.*;
+
 public class Main {
+    double radius;
+    Point center;
+
+    public Main(){
+        System.out.println();
+    }
+
+    public double getPerimeter(double length, double width){
+        double perimeter = (length + width) * 2;
+        return perimeter;
+    }
 
 }
